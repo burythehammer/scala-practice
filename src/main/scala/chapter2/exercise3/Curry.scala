6242592c1a30ba85cf421298da00b6dcec4b14a3
Matt Long <matt@burythehammer.com>
2018-04-08 22:44:10 +0100

    Chapter 2 exercise 3 - currying function

---
+++ src/main/scala/chapter2/exercise3/Curry.scala
@@ -0,0 +1,11 @@
+package chapter2.exercise3
+
+object Curry {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => {
+      (b: B) => {
+        f(a, b)
+      }
+    }
+}
+
---
+++ src/test/scala/chapter2/exercise3/CurrySpec.scala
@@ -0,0 +1,23 @@
+package chapter2.exercise3
+
+import org.scalatest.{FunSpec, Matchers}
+
+class CurrySpec extends FunSpec with Matchers {
+
+  describe("currying method") {
+
+    it("should curry addition") {
+      def addTwoNumbers(x: Int, y: Int): Int = x + y
+      val curriedFunction = Curry.curry(addTwoNumbers)
+      val addFiveToNumber = curriedFunction(5)
+      addFiveToNumber(20) should equal (25)
+    }
+
+    it("should curry string concatenation") {
+      def concatenateStrings(x: String, y: String): String = x.concat(y)
+      val curriedFunction = Curry.curry(concatenateStrings)
+      val attributeQuoteToShakespeare = curriedFunction("Shakespeare said: ")
+      attributeQuoteToShakespeare(" Hell is empty and all the devils are here") should equal ("Shakespeare said: Hell is empty and all the devils are here.")
+    }
+  }
+}
